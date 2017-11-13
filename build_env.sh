4c2b8b420bfb8eb5b83cad7b0335ebec76b24521
Ubuntu <ubuntu@ip-172-31-22-10.us-west-1.compute.internal>
2017-11-13 02:02:12 +0000

    some changes

--- build_env.sh
+++ build_env.sh
@@ -10,10 +10,10 @@ echo "Moving to tools"
 cd /home/ubuntu/caffe/build/tools
 
 echo "Generating training set"
-GLOG_logtostderr=1 ./convert_imageset --resize_height=227 --resize_width=227  /home/ubuntu/tcc_levi/tcc_dataset/train/ /home/ubuntu/tcc_levi/tcc_dataset/train.txt /mnt/tcc_data/train_leveldb 1
+GLOG_logtostderr=1 ./convert_imageset --resize_height=227 --resize_width=227  /home/ubuntu/tcc_levi/tcc_dataset/train/ /home/ubuntu/tcc_levi/tcc_dataset/train.txt /mnt/tcc_levi/train_leveldb 1
 
 echo "Generating validation db"
-GLOG_logtostderr=1 ./convert_imageset --resize_height=227 --resize_width=227  /home/ubuntu/tcc_levi/tcc_dataset/val/ /home/ubuntu/tcc_levi/tcc_dataset/val.txt /mnt/tcc_data/val_leveldb 1
+GLOG_logtostderr=1 ./convert_imageset --resize_height=227 --resize_width=227  /home/ubuntu/tcc_levi/tcc_dataset/val/ /home/ubuntu/tcc_levi/tcc_dataset/val.txt /mnt/tcc_levi/val_leveldb 1
 
 echo "Calculating mean"
-./compute_image_mean /mnt/tcc_levi/train_lmdb /mnt/tcc_levi/train_lmdb/mean_image.binaryproto
+./compute_image_mean /mnt/tcc_levi/train_leveldb /mnt/tcc_levi/train_leveldb/mean_image.binaryproto
